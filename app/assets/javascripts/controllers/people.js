d46154e12bcf8ff9f9efeec940247b9413f165e6
Miles Zimmerman <miles@zserver.org>
2014-06-17 18:40:07 -0700

    Can successfully edit people

--- app/assets/javascripts/controllers/people.js
+++ app/assets/javascripts/controllers/people.js
@@ -4,6 +4,74 @@
  */
 Antiquarium.Controller["people"] = {
 
-  // nothing
+  show: function() {
+    
+    // Gender
+    $('#gender').editable({
+      source: [
+        { value: 'unknown', text: 'unknown' },
+        { value: 'male',    text: 'male' },
+        { value: 'female',  text: 'female' }
+      ]
+    });
+    
+    // Nationality
+    $('#nationality').editable({
+      mode: 'popup',
+      source: Antiquarium.Nationalities
+    });
+    
+    // DOB
+    $('#date_of_birth').editable({
+      mode: 'popup',
+      placement: 'left',
+      combodate: {
+        minYear: 1700
+      }
+    });
+  
+    // DOD
+    $('#date_of_death').editable({
+      mode: 'popup',
+      placement: 'left',
+      combodate: {
+        minYear: 1700
+      }
+    });
+    
+    // Edit Name
+    $('#name').editable({
+      toggle: 'manual'
+    });
+    $('#name').on('shown', function(e, editable) {
+      $('#edit_name').hide();
+    });
+    $('#name').on('hidden', function(e, editable) {
+      $('#edit_name').show();
+    });
+    $('#edit_name').click(function(e) {
+      e.stopPropagation();
+      $('#name').editable('show');
+    });
+    
+    // Edit Description
+    $('#description').editable({
+      toggle: 'manual',
+      showbuttons: 'bottom'
+    });
+    $('#description').on('shown', function(e, editable) {
+      $('#edit_description').hide();
+    });
+    $('#description').on('hidden', function(e, editable) {
+      $('#edit_description').show();
+    });
+    $('#edit_description').click(function(e) {
+      e.stopPropagation();
+      $('#description').editable('show');
+    });
+    
+    // Initialize Cloudinary
+    Antiquarium.Common.cloudinaryUpload();
+  }
   
 };
