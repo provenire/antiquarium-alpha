b3818c50c43d084375afe1d619d249d9dac7e4f6
Miles Zimmerman <miles@zserver.org>
2014-07-04 07:16:27 -0700

    Cleaned up photo partials

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module Antiquarium
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     
+    # Load models
+    config.autoload_paths += Dir["#{config.root}/app/models/**/"]
+    
     
     # Load extra assets
     config.assets.paths << Rails.root.join("vendor","assets", "fonts")
